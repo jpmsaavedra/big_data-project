8ded813ff95a34645751ca7d926af3ce95202316
Joao Saavedra <joao_saavedra_18@hotmail.com>
2021-07-12 22:29:48 +0100

    SequentialProcessing function v2
    Modified SequentialProcessing function to clear load data code

--- SequentialProcessing.m
+++ SequentialProcessing.m
@@ -1,38 +1,14 @@
-%% This script allows you to open and explore the data in a *.nc file
-clear all
-close all
-
-FileName = '..\Model\o3_surface_20180701000000.nc';
-
-Contents = ncinfo(FileName);
+function [] = SequentialProcessing(FileName,HourlyData,NumHours,RadLat,RadLon,RadO3)
+%% Function to do sequential processing of model data
 
 Lat = ncread(FileName, 'lat'); % load the latitude locations
 Lon = ncread(FileName, 'lon'); % loadthe longitude locations
 
-%% Processing parameters provided by customer
-RadLat = 30.2016; % cluster radius value for latitude
-RadLon = 24.8032; % cluster radius value for longitude
-RadO3 = 4.2653986e-08; % cluster radius value for the ozone data
-
-%% Cycle through the hours and load all the models for each hour and record memory use
-% We use an index named 'NumHour' in our loop
 % The section 'sequential processing' will process the data location one
 % after the other, reporting on the time involved.
-
-StartLat = 1; % latitude location to start laoding
-NumLat = 400; % number of latitude locations ot load
-StartLon = 1; % longitude location to start loading
-NumLon = 700; % number of longitude locations ot load
 tic
-for NumHour = 1:25 % loop through each hour
-    fprintf('Processing hour %i\n', NumHour)
-    DataLayer = 1; % which 'layer' of the array to load the model data into
-    for idx = [1, 2, 4, 5, 6, 7, 8] % model data to load
-        % load the model data
-        HourlyData(DataLayer,:,:) = ncread(FileName, Contents.Variables(idx).Name,...
-            [StartLon, StartLat, NumHour], [NumLon, NumLat, 1]);
-        DataLayer = DataLayer + 1; % step to the next 'layer'
-    end
+for idxTime = 1:NumHours % loop through each hour
+    fprintf('Processing hour %i\n', idxTime)
     
     % We need to prepare our data for processing. This method is defined by
     % our customer. You are not required to understand this method, but you
@@ -48,7 +24,7 @@ for NumHour = 1:25 % loop through each hour
         % location. This method is defined by
         % our customer. You are not required to understand this method, but you
         % can ask your module leader for more information if you wish.
-        [EnsembleVector(idx, NumHour)] = EnsembleValue(Data2Process(idx,:,:,:), LatLon, RadLat, RadLon, RadO3);
+        [EnsembleVector(idx, idxTime)] = EnsembleValue(Data2Process(idx,:,:,:), LatLon, RadLat, RadLon, RadO3);
         
         % To monitor the progress we will print out the status after every
         % 50 processes.
@@ -59,11 +35,12 @@ for NumHour = 1:25 % loop through each hour
             t2 = toc;
         end
     end
-    T2(NumHour) = toc - t1; % record the total processing time for this hour
-    fprintf('Processing hour %i - %.2f s\n\n', NumHour, sum(T2));
+    T2(idxTime) = toc - t1; % record the total processing time for this hour
+    fprintf('Processing hour %i - %.2f s\n\n', idxTime, sum(T2));
     
         
 end
 tSeq = toc;
 
 fprintf('Total time for sequential processing = %.2f s\n\n', tSeq)
+end
